688cadcc1e67772fc651779a6ce2ca10fe241f8d
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-11 23:15:17 +0600

    Chapter 06: ExpressLetsStartBuildingtheNatoursAPI>> Lecture 06: StartingOurAPIHandlingGETRequests

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-//// Example:1
-// const express = require('express');
-
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.status(200).send('Hello from the server side!');
-// });
-// app.listen(port, () => {
-//   console.log(`Server listening on port ${port}`);
-// });
-// Example:2
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+    .json({ status: 'success', results: tours.length, data: { tours } });
 });
 app.post('/', (req, res) => {
   res.send('You can post to this end point');

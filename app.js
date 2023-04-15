32b7d2032bd26e5eeb47b4c6d2008fc4b69e7406
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-15 23:41:46 +0600

    Chapter 06: ExpressLetsStartBuildingtheNatoursAPI>> Lecture 16: CreatingandMountingMultipleRouters

--- app.js
+++ app.js
@@ -119,18 +119,16 @@ const deleteUser = (req, res) => {
     .json({ status: 'error', message: 'This route is not yet defined.' });
 };
 // 3. routes
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 //4. start servers
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);

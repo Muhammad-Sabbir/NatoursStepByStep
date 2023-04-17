acb1eba17ff1baa525a1cd8004b8236bb20b4ca3
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-17 22:54:26 +0600

    Chapter 06: ExpressLetsStartBuildingtheNatoursAPI>> Lecture 20: ServingStaticFiles

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const { application } = require('express');
+// const { application } = require('express');
 const app = express();
 
 // 1. middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!👌');

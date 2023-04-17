d19f0c2e3938eee402bf559d03d1648cb43f3312
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-17 22:53:22 +0600

    Chapter 06: ExpressLetsStartBuildingtheNatoursAPI>> Lecture 19: ChainingMultipleMiddlewareFunctions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+//check id middleware
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is : ${val}`);
   if (req.params.id * 1 > tours.length) {
@@ -13,7 +14,20 @@ exports.checkID = (req, res, next, val) => {
   }
   next(); // this must be the end of middleware function, otherwise it will be called again.
 };
-
+//check body middleware
+exports.checkBody = (req, res, next) => {
+  // if(!req.body.name||req.body.price){
+  if (
+    req.body.hasOwnProperty('name') == false ||
+    req.body.hasOwnProperty('price') == false
+  ) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Missing Name or Price',
+    });
+  }
+  next();
+};
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -34,7 +48,7 @@ exports.getTour = (req, res) => {
   });
 };
 exports.createTour = (req, res) => {
-  // console.log(req.body);
+  console.log(req.body);
   const newID = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newID }, req.body);
   tours.push(newTour);

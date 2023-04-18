e4339e25b7dc9c3f89825c8cec86b8e7a44d5c56
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-18 23:23:52 +0600

    Chapter 06: ExpressLetsStartBuildingtheNatoursAPI>> Lecture 21: EnvironmentVariables

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
-const port = 3000;
+// console.log(app.get('env')); // these are global variables
+// console.log(process.env);
+const port = 3000 || process.env.PORT;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+//power shell commands 'set NODE_ENV=development;node server.js'

482770bd69438cbdf4ec248ae5d44b3b72b4e36c
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-21 00:00:48 +0600

    Chapter 08: UsingMongoDBwithMongoose>> Lecture 8: AnotherWayofCreatingDocumentsPostRequest

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,22 +1,5 @@
 const Tour = require('./../models/tourModel');
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
 
-//check body middleware
-exports.checkBody = (req, res, next) => {
-  // if(!req.body.name||req.body.price){
-  if (
-    req.body.hasOwnProperty('name') == false ||
-    req.body.hasOwnProperty('price') == false
-  ) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Missing Name or Price',
-    });
-  }
-  next();
-};
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -36,13 +19,24 @@ exports.getTour = (req, res) => {
   //   data: { tour },
   // });
 };
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({}); //this method is older than
+    // newTour.save();
+
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'error',
+      message: err,
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({

6d9264ba6578bce65aa7a6adc11a1f517aad737d
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-30 14:05:57 +0600

    Chapter 08: UsingMongoDBwithMongoose>> Lecture 20 Part-1: Refactoring API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,68 +1,72 @@
 const Tour = require('../models/tourModel');
+
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
-exports.getAllTours = async (req, res) => {
-  try {
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query }; // three dot structuring an object shallow copy here
+
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString }; // three dot structuring an object shallow copy here
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
     // there are some special query string that will be used to find or for paginations and not actually query into the database. So we will have to specifi these special query string saperately.
 
     //1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
-    console.log(queryStr);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this; // means, return this object
+  }
 
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    // Example -1
-    // if (req.query.sort) {
-    //   query = query.sort(req.query.sort);
-    // }
-
-    // Example -2: Multiple sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy);
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
     } else {
-      query = query.sort('createdAt');
+      this.query = this.query.sort('-createdAt');
     }
-    // Filtering Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+    return this; // means, return this object
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
     } else {
-      query = query.select('-__v');
+      this.query = this.query.select('-__v');
     }
+    return this; // means, return this object
+  }
 
-    //Pagination
-    const page = req.query.page * 1 || 1; // '123'*1 = 123 this is how convert a string to a number in js. and Here default page is 1.
-    const limit = req.query.limit * 1 || 100;
-
+  paginate() {
+    const page = this.queryString.page * 1 || 1; // '123'*1 = 123 this is how convert a string to a number in js. and Here default page is 1.
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
-
     //Example:
     // page=3&limit=10, 1-10--> page 1, 11-20--> page 2, 21-30--> page3
     // query = query.skip(20).limit(10);
+    this.query = this.query.skip(skip).limit(limit);
+    return this; // means, return this object
+  }
+}
 
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
+exports.getAllTours = async (req, res) => {
+  try {
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     //query.sort().select().skip().limit() // this is a big query. We can add multiple statements to the query
 
     // SEND RESPONSE

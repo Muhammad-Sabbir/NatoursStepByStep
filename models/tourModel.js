d2f846ead9e01fb9316f623aadcc21823e370eac
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-05-02 21:22:05 +0600

    Chapter 08: UsingMongoDBwithMongoose>> Lecture 24: DocumentMiddleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 // const opts = { toObject: { virtuals: true } };
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -63,7 +66,56 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7; // here we use function instead of arrow function, becused arrow function does not support this keyword // fuck this line, cause this line takes my extra 1 hour!!! shittttttttttt
 });
-// tourSchema.set('toJSON', { virtuals: true });
-// tourSchema.set('toObject', { virtuals: true });
+
+// DOCUMENT MIDDLEWARE: rund before .save() and .create()
+//Example-1:
+// tourSchema.pre('save', function () {
+//   console.log(this);
+// });
+// Example-2:
+// tourSchema.pre('save', function (next) {
+//   // this is a pre middleware
+//   this.slug = slugify(this.name, { lower: true });
+//   next(); // because this is a middleware, so next()
+// });
+
+// Example-3: there can be multiple pre middleware
+// tourSchema.pre('save', function (next) {
+//   // this is the first pre middleware
+//   this.slug = slugify(this.name, { lower: true });
+//   next(); // because this is a middleware, so next()
+// });
+
+// tourSchema.pre('save', (next) => {
+//   // this is the second pre middleware
+//   console.log('Will save document.....');
+//   next(); // because this is a middleware, so next()
+// });
+
+// Example-4: there can be multiple pre middleware
+// tourSchema.pre('save', function (next) {
+//   // this is the first pre middleware
+//   this.slug = slugify(this.name, { lower: true });
+//   next(); // because this is a middleware, so next()
+// });
+
+// tourSchema.pre('save', (next) => {
+//   // this is the second pre middleware
+//   console.log('Will save document.....');
+//   next(); // because this is a middleware, so next()
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+// let's comment the console.log functions
+tourSchema.pre('save', function (next) {
+  // this is the first pre middleware
+  this.slug = slugify(this.name, { lower: true });
+  next(); // because this is a middleware, so next()
+});
+
 const Tour = mongoose.model('Tour', tourSchema); // First letter of model variable name must be capital letter. here Tour, T is in capital letter.
 module.exports = Tour;

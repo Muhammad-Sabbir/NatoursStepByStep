ef4bc31ddc9f0811f2e1a8c69a56a40da3b4d00a
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-05-03 19:27:16 +0600

    Chapter 09: ErrorHandlingwithExpress>> Lecture 03:HandlingUnhandledRoutes

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!👌');
-  next();
-});
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -26,4 +23,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // this block must be after all routs.
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;

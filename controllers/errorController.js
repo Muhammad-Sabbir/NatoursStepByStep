6d78b0647fc56745a375b20feae3382a30a565c4
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-05-09 21:29:12 +0600

    Chapter 09: ErrorHandlingwithExpress>> Lecture 09: ErrorsDuringDevelopmentvsProduction

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,42 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send messsage to client.
+  if (err.isOperationError) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  //Programming or other unknown error: don't leak error details to client
+  else {
+    //1) Log error
+    console.error('ERROR 🙈');
+
+    //2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack); // actualy will show us where the error happened
   //default error status code
   err.statusCode = err.statusCode || 500; //which mean internal server error
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+  if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };

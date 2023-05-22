b00658f1a4cd8038eba7c9865230b28d7d47e511
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-05-22 23:19:52 +0600

    Chapter 09: ErrorHandlingwithExpress>> Lecture 16: UpdatingtheCurrentUserData

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 const User = require('../models/userModel');
 // eslint-disable-next-line import/no-unresolved
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -15,6 +24,41 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: { users },
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data.
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  // 2) Update user document
+  // ex1: here we can not use this process because this process required the password.
+  // const user = await User.findById(req.user.id);
+  // user.name = 'Jonas';
+  // await user.save();
+
+  // Ex2:
+  // Filtered out unwanted fields names that are not allowed to be updated.
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res
     .status(500)
--- models/userModel.js
+++ models/userModel.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valide email'],
+    validator: [validator.isEmaiil, 'Please provide a valide email'],
   },
   photo: {
     type: String,

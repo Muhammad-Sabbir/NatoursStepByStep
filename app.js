146974a3ccf8aa0556470208b2d503f2908cd562
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-05-31 21:09:59 +0600

    Chapter 10: AuthenticationAuthorizationandSecurity>> Lecture 21: SettingSecurityHTTPHeaders

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet'); // https://github.com/helmetjs/helmet
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,10 +12,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1. Global middlewares
+// Set security HTTP headers
+app.use(helmet());
 console.log(process.env.NODE_ENV);
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -22,9 +29,13 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body int req.body
+app.use(express.json({ limit: '10kb' })); // the package will then understand it will parse this string here into a meaningful data, and so now when we have a body larger than 10 kilobyte it will not be accepted
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);

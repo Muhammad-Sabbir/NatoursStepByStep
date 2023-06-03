2674c1410492cc8db9fb30489a4d8f01bab8fcc3
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-06-03 23:38:43 +0600

    Chapter 10: AuthenticationAuthorizationandSecurity>> Lecture 22: DataSanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet'); // https://github.com/helmetjs/helmet
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,13 +34,18 @@ app.use('/api', limiter);
 // Body parser, reading data from body int req.body
 app.use(express.json({ limit: '10kb' })); // the package will then understand it will parse this string here into a meaningful data, and so now when we have a body larger than 10 kilobyte it will not be accepted
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize()); // QUERY_SANITIZATION
+
+// Data sanitization against XSS
+app.use(xss()); // if any attacker inject some html file with JS code then this will converting all these HTML Symbols
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
+  // console.log(req.headers);
   next();
 });
 

03bfd38a329b2bed992b08e1f571a625271cd83e
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-06-28 22:31:04 +0600

    Chapter 11: ModellingDataandAdvancedMongoose>> Lecture 12: ImplementingSimpleNestedRoutes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -28,4 +29,17 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
   ); // Protect: if a user is logged in..// restrictTo: authorization
+
+// POST /tour/4u5u5u/reviews
+//GET /tour/4u5u5u/reviews
+//GET /tour/4u5u5u/reviews/RU5U58T
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
+
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,4 +25,5 @@ router
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
+
 module.exports = router;

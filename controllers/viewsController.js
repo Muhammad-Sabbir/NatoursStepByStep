95945201f7b381cc7838e04a4bda1e59d3171281
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-08-22 22:19:47 +0600

    Chapter 12: Server-SideRenderingwithPugTemplates >> Lecture 8: SettinguptheProjectStructure

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,36 +1,11 @@
-const Tour = require('../models/tourModel');
-const catchAsync = require('../utils/catchAsync');
-
-exports.getOverview = catchAsync(async (req, res) => {
-  // 1) Get tour data from collection
-  const tours = await Tour.find();
-
-  // 2) Build template
-
-  // 3) Render that template using tour data from 1)
+exports.getOverview = (req, res) => {
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours,
   });
-});
+};
 
-exports.getTour = catchAsync(async (req, res) => {
-  // 1) Get the data, for the requested tour (including reviews and guides)
-  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-    path: 'reviews',
-    fields: 'review rating user',
+exports.getTour = (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
   });
-
-  // 2) Build template
-  //3) Render template using data from 1)
-  res
-    .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
-    .render('tour', {
-      title: `${tour.name} tour`,
-      tour,
-    });
-});
+};
